2b7101c68fffdff2b7b0ac1fa96cbb2f7fe8084a
Snowy Dr3amer <tofu@dr3amer.de>
2016-04-19 16:51:19 +0200

    added some stuff

--- cgvbeleg/src/math/Vektor2D.java
+++ cgvbeleg/src/math/Vektor2D.java
@@ -7,87 +7,124 @@ public class Vektor2D {
 	public Vektor2D( ) {
 		this( 0.0, 0.0 );
 	}
+
 	// ********************************************//
 	public Vektor2D( double x, double y ) {
 		this.x = x;
 		this.y = y;
 	}
+
 	// ********************************************//
 	public Vektor2D( Vektor2D src ) {
 		this.x = src.x;
 		this.y = src.y;
 	}
+
 	// ********************************************//
 	public String toString( ) {
 		return "x: " + this.x + " y: " + this.y;
 	}
+
 	// ********************************************//
 	public double[ ] getArray( ) {
 		double[ ] arr = { this.x, this.y };
 		return arr;
 	}
+
 	// ********************************************//
 	public void setX( double x ) {
 		this.x = x;
 	}
+
 	// ********************************************//
 	public double getX( ) {
 		return this.x;
 	}
+
 	// ********************************************//
 	public void setY( double y ) {
 		this.y = y;
 	}
+
 	// ********************************************//
 	public double getY( ) {
 		return this.y;
 	}
+
 	// ********************************************//
 	public void setPosition( double x, double y ) {
 		this.x = x;
 		this.y = y;
 	}
-	// ********************************************//
-	public boolean isNullVector( ) {
-		return ( this.x == 0 && this.y == 0 );
-	}
+
 	// ********************************************//
 	public void add( Vektor2D src ) {
 		this.x += src.x;
 		this.y += src.y;
 	}
+
+	// ********************************************//
+	public void add( double src ) {
+		this.x += src;
+		this.y += src;
+	}
+
 	// ********************************************//
 	public void sub( Vektor2D src ) {
 		this.x -= src.x;
 		this.y -= src.y;
 	}
+
+	// ********************************************//
+	public void sub( double src ) {
+		this.x -= src;
+		this.y -= src;
+	}
+
 	// ********************************************//
 	public void mult( Vektor2D src ) {
 		this.x *= src.x;
 		this.y *= src.y;
 	}
+
 	// ********************************************//
 	public void mult( double src ) {
 		this.x *= src;
 		this.y *= src;
 	}
+
 	// ********************************************//
 	public void div( Vektor2D src ) {
 		this.x /= src.x;
 		this.y /= src.y;
 	}
+
+	// ********************************************//
+	public void div( double src ) {
+		this.x /= src;
+		this.y /= src;
+	}
+
+	// ********************************************//
+	public boolean isNullVector( ) {
+		return ( this.x == 0 && this.y == 0 );
+	}
+
 	// ********************************************//
 	public boolean isEqual( Vektor2D src ) {
 		return ( this.x == src.x && this.y == src.y );
 	}
+
 	// ********************************************//
 	public boolean isNotEqual( Vektor2D src ) {
 		return ( this.x != src.x || this.y != src.y );
 	}
+
 	// ********************************************//
 	public double length( ) {
 		return Math.sqrt( ( this.x * this.x ) + ( this.y * this.y ) );
 	}
+
 	// ********************************************//
 	public void normalize( ) {
 		mult( 1 / length( ) );
--- cgvbeleg/tests/testspackage/Vektor2DTests.java
+++ cgvbeleg/tests/testspackage/Vektor2DTests.java
@@ -9,14 +9,14 @@ public class Vektor2DTests {
 	public void testAddVektoren( ) {
 		// Initializations.
 		Vektor2D vec1 = new Vektor2D( 1.0, 2.0 );
-		Vektor2D vec2 = new Vektor2D( 1.0, 2.0 );
-		Vektor2D erg = new Vektor2D( 2.0, 4.0 );
+		Vektor2D vec2 = new Vektor2D( 2.0, 2.0 );
+		Vektor2D erg1 = new Vektor2D( 3.0, 4.0 );
 
 		// Methods.
 		vec1.add( vec2 );
 
 		// Tests.
-		assertArrayEquals( vec1.getArray( ), erg.getArray( ), 0.001 );
+		assertArrayEquals( erg1.getArray( ), vec1.getArray( ), 0.001 );
 	}
 
 	@Test
@@ -24,12 +24,98 @@ public class Vektor2DTests {
 		// Initializations.
 		Vektor2D vec1 = new Vektor2D( 3.0, 4.0 );
 		Vektor2D vec2 = new Vektor2D( 1.0, 2.0 );
-		Vektor2D erg = new Vektor2D( 2.0, 2.0 );
+		Vektor2D erg1 = new Vektor2D( 2.0, 2.0 );
 
 		// Methods.
 		vec1.sub( vec2 );
 
 		// Tests.
-		assertArrayEquals( vec1.getArray( ), erg.getArray( ), 0.001 );
+		assertArrayEquals( erg1.getArray( ), vec1.getArray( ), 0.001 );
+	}
+
+	@Test
+	public void testMultVektoren( ) {
+		// Initializations.
+		Vektor2D vec1 = new Vektor2D( 1.0, 2.0 );
+		Vektor2D vec2 = new Vektor2D( 2.0, 2.0 );
+		Vektor2D erg1 = new Vektor2D( 2.0, 4.0 );
+
+		// Methods.
+		vec1.mult( vec2 );
+
+		// Tests.
+		assertArrayEquals( erg1.getArray( ), vec1.getArray( ), 0.001 );
+	}
+	
+	@Test
+	public void testDivVektoren( ) {
+		// Initializations.
+		Vektor2D vec1 = new Vektor2D( 2.0, 2.0 );
+		Vektor2D vec2 = new Vektor2D( 2.0, 2.0 );
+		Vektor2D erg1 = new Vektor2D( 1.0, 1.0 );
+
+		// Methods.
+		vec1.div( vec2 );
+
+		// Tests.
+		assertArrayEquals( erg1.getArray( ), vec1.getArray( ), 0.001 );
+	}
+	
+	@Test
+	public void testIsNullVektoren( ) {
+		// Initializations.
+		Vektor2D vec1 = new Vektor2D( 0.0, 0.0 );
+		Vektor2D vec2 = new Vektor2D( 2.0, 2.0 );
+
+		// Tests.
+		assertTrue( vec1.isNullVector( ) );
+		assertFalse( vec2.isNullVector( ) );
+	}
+	
+	@Test
+	public void testIsEqualVektoren( ) {
+		// Initializations.
+		Vektor2D vec1 = new Vektor2D( 1.0, 1.0 );
+		Vektor2D vec2 = new Vektor2D( 1.0, 1.0 );
+		Vektor2D vec3 = new Vektor2D( 2.0, 2.0 );
+
+		// Tests.
+		assertTrue( vec1.isEqual( vec2 ) );
+		assertFalse( vec1.isEqual( vec3 ) );
+	}
+	
+	@Test
+	public void testIsNotEqualVektoren( ) {
+		// Initializations.
+		Vektor2D vec1 = new Vektor2D( 1.0, 1.0 );
+		Vektor2D vec2 = new Vektor2D( 1.0, 1.0 );
+		Vektor2D vec3 = new Vektor2D( 2.0, 2.0 );
+
+		// Tests.
+		assertFalse( vec1.isNotEqual( vec2 ) );
+		assertTrue( vec1.isNotEqual( vec3 ) );
+	}
+	
+	@Test
+	public void testLengthVektoren( ) {
+		// Initializations.
+		Vektor2D vec1 = new Vektor2D( 2.0, 0.0 );
+		double erg1 = 2.0;
+
+		// Tests.
+		assertEquals( erg1, vec1.length( ), 0.001 );
+	}
+	
+	@Test
+	public void testNormalizeVektoren( ) {
+		// Initializations.
+		Vektor2D vec1 = new Vektor2D( 2.0, 0.0 );
+		Vektor2D erg1 = new Vektor2D( 1.0, 0.0 );
+
+		// Methods.
+		vec1.normalize( );
+		
+		// Tests.
+		assertArrayEquals( erg1.getArray( ), vec1.getArray( ), 0.001 );
 	}
 }
